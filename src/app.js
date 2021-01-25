7b514b62678f352e1b8a3271c996feb87af11cd4
yinliguo <yinlg@outlook.com>
2021-01-25 16:13:56 +0800

    Flipair

--- src/app.js
+++ src/app.js
@@ -1,63 +1,77 @@
-import "./reset.scss";
-import "./app.scss";
-import "swiper/swiper.scss";
-import Swiper, {Pagination, Autoplay} from "swiper";
+import './reset.scss';
+import './app.scss';
+import 'swiper/swiper.scss';
+import Swiper, { Pagination, Autoplay } from 'swiper';
 
-const isDevelopment = process.env.NODE_ENV == "development";
+const isDevelopment = process.env.NODE_ENV == 'development';
 
 Swiper.use([Pagination, Autoplay]);
 
 const apps = [
   {
-    name: "Danger Around",
-    dir:  (isDevelopment ? "" : "/apps") + "/danger-around/",
-    icon: "logo.png",
+    name: 'Danger Around',
+    dir: (isDevelopment ? '' : '/apps') + '/danger-around/',
+    icon: 'logo.png',
     desc:
-      "Life is in a circle, facing uncertainties, bad or good, so we should learn how to avoid danger, pursue happiness.",
+      'Life is in a circle, facing uncertainties, bad or good, so we should learn how to avoid danger, pursue happiness.',
     screenshots: [
-      "5.5-1.png",
-      "5.5-2.png",
-      "5.5-3.png",
-      "5.5-4.png",
-      "5.5-5.png",
-      "5.5-6.png",
+      '5.5-1.png',
+      '5.5-2.png',
+      '5.5-3.png',
+      '5.5-4.png',
+      '5.5-5.png',
+      '5.5-6.png',
     ],
+  },
+  {
+    name: 'Flipart',
+    dir: (isDevelopment ? '' : '/apps') + '/flipair/',
+    icon: 'logo.png',
+    desc:
+      'Remember card contents, find the same cards. You can use some special memory methods to earn more score. The game aim to train your working memory. 2P mode supports you play with your family or friends.',
+    screenshots: ['5.5-1.png', '5.5-2.png', '5.5-3.png'],
   },
 ];
 
 const ScreenshotRatio = 16 / 9;
 
-const iconListEl = document.getElementsByClassName("icon-list").item(0);
-const descEl = document.getElementsByClassName("app-desc").item(0);
-const swiperContainerEl = document.getElementsByClassName('swiper-container').item(0);
+const iconListEl = document.getElementsByClassName('icon-list').item(0);
+const descEl = document.getElementsByClassName('app-desc').item(0);
+const swiperContainerEl = document
+  .getElementsByClassName('swiper-container')
+  .item(0);
 
-function resetSwiperContainerHeight()
-{
-  swiperContainerEl.style.height = swiperContainerEl.offsetWidth / ScreenshotRatio + 'px';
+function resetSwiperContainerHeight() {
+  swiperContainerEl.style.height =
+    swiperContainerEl.offsetWidth / ScreenshotRatio + 'px';
 }
 resetSwiperContainerHeight();
 
-const myswiper = new Swiper(".swiper-container", {
+const myswiper = new Swiper('.swiper-container', {
   speed: 400,
   autoplay: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
   },
 });
 
 apps.forEach((item) => {
-  const iconEl = document.createElement("li");
-  iconEl.className = "icon";
+  const iconEl = document.createElement('li');
+  iconEl.className = 'icon';
   iconEl.style.backgroundImage = `url(${item.dir + item.icon})`;
   iconListEl.append(iconEl);
 
-  iconEl.addEventListener("click", () => {
+  iconEl.addEventListener('click', () => {
     descEl.textContent = item.desc;
     if (myswiper.slides) {
       myswiper.removeAllSlides();
     }
     item.screenshots.forEach((ss) => {
-      myswiper.appendSlide(`<div class="swiper-slide" style="background-image: url(${item.dir + ss})"></div>`);
+      myswiper.appendSlide(
+        `<div class="swiper-slide" style="background-image: url(${
+          item.dir + ss
+        })"></div>`
+      );
     });
     myswiper.update();
     myswiper.slideTo(0);
